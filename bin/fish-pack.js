f55a8190ee426ac03ac440aa98b1bfd4bb082b36
lok660 <lokk7991@gmail.com>
2020-09-24 01:35:11 +0800

    feat:完成ejs模板

--- bin/fish-pack.js
+++ bin/fish-pack.js
@@ -5,7 +5,7 @@ const path = require('path')
 const config = require(path.join(__dirname, '../webpack.config.js'))
 
 // 找到项目目录下的 webpack.config.js 然后通过一个 compiler 解析它
-const Complier = require('../lib/compiler.js')
+const Complier = require('../lib/Compiler.js')
 const complier = new Complier(config)
 
 // 在 run 之前发布一个 entryOptions 的钩子
