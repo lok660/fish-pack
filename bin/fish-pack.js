ab1cca58b3863113b84a942eaffb657086c66031
lok660 <lokk7991@gmail.com>
2020-09-23 23:26:05 +0800

    feat:解析读取AST

--- bin/fish-pack.js
+++ bin/fish-pack.js
@@ -1,3 +1,14 @@
 #! /usr/bin/env node
 
-console.log('hello world')
+const path = require('path')
+const config = require(path.resolve(__dirname))
+
+// 找到项目目录下的 webpack.config.js 然后通过一个 compiler 解析它
+const Complier = require('../lib/compiler.js')
+const complier = new Complier(config)
+
+// 在 run 之前发布一个 entryOptions 的钩子
+// complier.hooks.entryOptions.call()
+
+// 调用 compiler 的 run 方法执行代码
+complier.run()
